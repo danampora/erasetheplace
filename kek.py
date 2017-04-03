8016006eed50fa1c69fcd5d5d5a8fd00ec973133
IncognitoJam <IncognitoJam@users.noreply.github.com>
2017-04-03 12:06:03 +0100

    Update kek.py

--- kek.py
+++ kek.py
@@ -1,3 +1,7 @@
+##########################################
+####### THIS CODE IS DEPRECATED :( #######
+##########################################
+
 # Hit run at the top!
 # This program will not save your reddit login details.
 
